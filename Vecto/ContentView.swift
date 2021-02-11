a25187470afcbc9ea1b17324fb3907b4b0b8e9a4
TheEnquirer <huxmarv@nuevaschool.org>
2021-02-10 22:15:42 -0800

    for sure

---
+++ Vecto/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vecto
+//
+//  Created by Huxley Marvit on 2/10/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vecto/VectoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VectoApp.swift
+//  Vecto
+//
+//  Created by Huxley Marvit on 2/10/21.
+//
+
+import SwiftUI
+
+@main
+struct VectoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
