afb4c9d66a5f8d39187575c678cc2be21efa4290
TheEnquirer <huxmarv@nuevaschool.org>
2021-02-11 16:31:09 -0800

    displaying pdfs baby!

--- Vecto/Vecto/ViewController.swift
+++ Vecto/Vecto/ViewController.swift
@@ -6,11 +6,44 @@
 //
 
 import Cocoa
+import PDFKit
 
 class ViewController: NSViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+//        let betterBounds = (0, 0, 1480, 1270)
+////        self.view.setBoundsSize(n)
+//        let pdfView = PDFView(frame: self.view.bounds)
+//        print(self.view.bounds)
+////            pdfView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+//            self.view.addSubview(pdfView)
+//
+//            // Fit content in PDFView.
+//            pdfView.autoScales = true
+//
+//            // Load Sample.pdf file from app bundle.
+////            let fileURL = Bundle.main.url(forResource: "Sample", withExtension: "pdf")
+////            pdfView.document = PDFDocument(url: fileURL!)
+//        pdfView.document = PDFDocument(url: URL(string: "~/Desktop/here.pdf")!)
+
+        
+        let pdfView = PDFView()
+
+        pdfView.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(pdfView)
+
+        pdfView.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        pdfView.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        pdfView.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        pdfView.bottomAnchor.constraint(equalTo: view.bottomAnchor).isActive = true
+        
+        guard let path = Bundle.main.url(forResource: "here1", withExtension: "pdf") else { return }
+//        let path = URL(string: "~/Desktop/here.pdf")!
+
+        if let document = PDFDocument(url: path) {
+            pdfView.document = document
+        }
 
         // Do any additional setup after loading the view.
     }
